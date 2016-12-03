fe5f37288f711dddb974e37759e7db8343b5270f
Sean Clayton 🎷🐢 <sclayton@swifttrip.com>
2016-12-02 22:49:45 -0500

    Add space-age answer

---
+++ elixir/space-age/space_age.exs
@@ -0,0 +1,25 @@
+defmodule SpaceAge do
+  @type planet :: :mercury | :venus | :earth | :mars | :jupiter
+                | :saturn | :uranus | :neptune
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(planet, seconds) do
+    # Define all planets with their year in seconds
+    planets = %{
+      :mercury => 7600543.81992,
+      :venus => 19414149.052176,
+      :earth => 31557600,
+      :mars => 59354032.69008,
+      :jupiter => 374355659.124,
+      :saturn => 929292362.8848,
+      :uranus => 2651370019.3296,
+      :neptune => 5200418560.032
+    }
+    
+    seconds / planets[planet]
+  end
+end
