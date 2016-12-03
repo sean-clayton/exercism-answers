96c88c4da0e9ee111e52a2611b85149179a31cdc
Sean Clayton 🎷🐢 <sclayton@swifttrip.com>
2016-12-02 23:14:02 -0500

    Add solution for bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,8 +1,31 @@
 defmodule Bob do
+  @spec hey(charlist) :: charlist
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
-
+      doesnt_say_anything?(input) ->
+        "Fine. Be that way!"
+      is_a_question?(input) ->
+        "Sure."
+      yelling?(input) ->
+        "Whoa, chill out!"
+      true ->
+        "Whatever."
     end
+  end
+
+  @spec yelling?(charlist) :: boolean
+  def yelling?(message) do
+    lowercase_message = String.downcase(message) # Solves for sentences without letters
+    message == String.upcase(message) && message != lowercase_message
+  end
+
+  @spec is_a_question?(charlist) :: boolean
+  def is_a_question?(message) do
+    String.ends_with?(message, "?")
+  end
+
+  @spec doesnt_say_anything?(charlist) :: boolean
+  def doesnt_say_anything?(message) do
+    String.trim(message) == ""
   end
 end
